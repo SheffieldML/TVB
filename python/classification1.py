63193a93314e8e0551c49d7a22a9c202a9790e15
maxz <ibinbei@gmail.com>
2013-09-11 12:11:23 +0100

    still dB_dbeta bc of wrong objective

--- python/classification1.py
+++ python/classification1.py
@@ -2,6 +2,7 @@ import numpy as np
 import pylab as pb
 import GPy
 from truncnorm import truncnorm
+from GPy.models.gradient_checker import GradientChecker
 
 class classification(GPy.core.Model):
     def __init__(self, X, Y, kern):
@@ -52,7 +53,8 @@ class classification(GPy.core.Model):
     def log_likelihood(self):
         #expectation of log pseudo-likelihood times prior under q
         A = -self.num_data*np.log(2*np.pi) + 0.5*np.log(self.beta).sum() - 0.5*self.K_logdet
-        A += -0.5*np.sum(self.beta*(np.square(self.Ytilde - self.q_means) + self.q_vars))
+        # A += -0.5*np.sum(self.beta*(np.square(self.Ytilde - self.q_means) + self.q_vars))
+        A += -0.5 * np.sum(self.beta * (np.square(self.Ytilde) + self.q_vars))
         tmp, _ = GPy.util.linalg.dtrtrs(self.L,self.q_means, lower=1)
         A += -0.5*np.sum(np.square(tmp)) - 0.5*np.sum(np.diag(self.Ki)*self.q_vars)
 
@@ -66,24 +68,32 @@ class classification(GPy.core.Model):
         return B
 
     def _log_likelihood_gradients(self):
+        # first compute gradients wrt cavity means/vars, then chain
+        dq_dcav_means = np.array([q.dH_dmu() for q in self.truncnorms])
+        dq_dcav_vars = np.array([q.dH_dvar() for q in self.truncnorms])
 
         #A TODO
-        dA_dYtilde = 0
+        dA_dYtilde = 0  # self.beta * (self.Ytilde - self.q_means)
         dA_dbeta = 0
 
         #B
-        #first compute gradients wrt cavity means/vars, then chain
-        dB_dcav_means = np.array([q.dH_dmu() for q in self.truncnorms])
-        dB_dcav_vars = np.array([q.dH_dvar() for q in self.truncnorms])
-
         #watch the broadcast!
         dcav_vars_dbeta = -(self.Sigma**2 / self.diag_Sigma**2 - np.eye(self.num_data) )*self.cavity_vars**2
-        dcav_means_dbeta = (self.Ytilde[:, None] - self.cavity_means) * self.Sigma  # np.eye(self.num_data)#TODO
-        #dcav_vars_dYtilde = 0
-        dcav_means_dYtilde = (self.Sigma*self.beta[:,None]/self.diag_Sigma - np.diag(self.beta))*self.cavity_vars
+        dcav_means_dbeta_l = dcav_vars_dbeta * (self.mu / self.diag_Sigma - self.Ytilde * self.beta)
+        dmu_dbeta = (self.Ytilde[:, None] - self.cavity_means) * self.Sigma  # np.eye(self.num_data)#TODO
+        dcav_means_dbeta_r = self.cavity_vars * ((dmu_dbeta / self.diag_Sigma + self.mu * (self.Sigma / self.diag_Sigma) ** 2) - (self.Ytilde * np.eye(self.beta.shape[0])))
+        dcav_means_dbeta = dcav_means_dbeta_l + dcav_means_dbeta_r
 
-        dB_dbeta = np.dot(dcav_means_dbeta, dB_dcav_means) + np.dot(dcav_vars_dbeta, dB_dcav_vars)
-        dB_dYtilde = np.dot(dcav_means_dYtilde, dB_dcav_means)
+        dcav_means_dbeta = self.cavity_vars ** 2 * ((self.Sigma / self.diag_Sigma) ** 2 - np.eye(self.beta.shape[0])) * ((self.mu / self.diag_Sigma) - self.Ytilde * self.beta)
+        dcav_means_dbeta += self.cavity_vars * self.Sigma / self.diag_Sigma * (self.Ytilde[:, None] - self.mu[:, None])
+        dcav_means_dbeta += self.mu * self.cavity_vars * (self.Sigma / self.diag_Sigma) ** 2
+        dcav_means_dbeta -= self.cavity_means * self.Ytilde * np.eye(self.beta.shape[0])
+
+        #dcav_vars_dYtilde = 0
+        dcav_means_dYtilde = (self.Sigma * self.beta[:, None] / self.diag_Sigma - np.diag(self.beta)) * self.cavity_vars
+
+        dB_dbeta = np.dot(dcav_means_dbeta, dq_dcav_means) + np.dot(dcav_vars_dbeta, dq_dcav_vars)
+        dB_dYtilde = np.dot(dcav_means_dYtilde, dq_dcav_means)
 
 
         #C TODO
@@ -106,15 +116,25 @@ class classification(GPy.core.Model):
 
 
 if __name__=='__main__':
-    N = 50
+    N = 2
     X = np.random.rand(N)[:,None]
     X = np.sort(X,0)
     Y = np.where(X>0.5,1,0).flatten()
-    m = classification(X,Y,GPy.kern.rbf(1,1,0.2) + GPy.kern.white(1,1e-1))
+    m = classification(X, Y, GPy.kern.rbf(1, .6, 0.2) + GPy.kern.white(1, 1e-1))
     m.constrain_positive('beta')
     m.constrain_fixed('rbf')
     m.constrain_fixed('white')
     #m.optimize('simplex', max_f_eval=20000, messages=1)
+    m.randomize()
     m.checkgrad(verbose=True)
 
-
+#     def f(b):
+#         m['beta'] = b
+#         m.log_likelihood()
+#         return m.cavity_means[0]
+#     def df(b):
+#         m['beta'] = b
+#         m._log_likelihood_gradients()
+#         return m.dcav_means_dbeta[0]
+#     mu_grad = GradientChecker(f, df, m.beta, ['beta'])
+#     mu_grad.checkgrad(verbose=1)

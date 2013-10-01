9f5e254304cb21c7b4ee222f03696f46886a9e3f
James Hensman <james.hensman@gmail.com>
2013-10-01 20:39:13 +0100

    minor changes to dataset comparison

--- python/compare_datasets_plot_results.py
+++ python/compare_datasets_plot_results.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib
-#matplotlib.use('pdf')
+matplotlib.use('pdf')
 import pylab as pb
-pb.ion()
+#pb.ion()
 import os
 from scipy import stats
 
@@ -16,18 +16,20 @@ stds = np.vstack([stats.nanstd(e, 0) for e in data])
 x = np.arange(len(data))
 width=0.35
 
-pb.figure()
+pb.figure(figsize=(10,4))
 error_kw = {'elinewidth':1.2, 'ecolor':'k', 'capsize':5, 'mew':1.2}
 pb.bar(x, means[:,1], yerr=stds[:,1], width=width, color='b', label='var_EP', error_kw=error_kw)
 pb.bar(x+width, means[:,5], yerr=stds[:,5], color='r', width=width, label='EP', error_kw=error_kw)
-pb.xticks(x+width,[fn.split('raw')[0] for fn in fnames])
+pb.xticks(x+width,[fn.split('raw')[0] for fn in fnames], rotation=45)
 
 for xx, m, s in zip(x, means[:,1], stds[:,1]):
-    pb.text(xx+0.5*width, 1.0*(m+s), '%.3f'%m,ha='center', va='bottom')
+    pb.text(xx+0.5*width, 1.0*(m+s), '%.3f'%m,ha='center', va='bottom', fontsize='small')
 for xx, m, s in zip(x+width, means[:,5], stds[:,5]):
-    pb.text(xx+0.5*width, 1.0*(m+s), '%.3f'%m,ha='center', va='bottom')
+    pb.text(xx+0.5*width, 1.0*(m+s), '%.3f'%m,ha='center', va='bottom', fontsize='small')
 
 #pb.legend(loc=0)
-pb.ylim(0,0.7)
+pb.ylim(0,1.05*np.max(means[:,[1,5]].flatten() + stds[:,[1,5]].flatten()))
+pb.ylabel(r'$-\log\, p(y_\star)$')
+pb.subplots_adjust(bottom=0.2)
 
 pb.savefig('nlps.pdf')
